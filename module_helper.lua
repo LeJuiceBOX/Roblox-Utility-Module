89b37431fe59dc5402bb00654d72a380bf9a9774
DavidRHunt <19377840+LeJuiceBOX@users.noreply.github.com>
2020-06-08 18:36:13 -0400

    Update module_helper.lua

--- module_helper.lua
+++ module_helper.lua
@@ -3,24 +3,26 @@
 --// Desc:		Helper functions, used inside the main module.                                                             
 --======================================================================================================
 
-local Config = script.Parent.Configuration
+local Main = script.Parent.Parent
+local Config = Main.Configuration
+local Data = Main.Data
 
 local module = {}
 	
-	module.PluginPrint = function(cont,t)
-		local text = "[ RUM ] - "..tostring(cont)  
+	module.ModPrint = function(cont,t)
+		local text = Config.PrintName.Value " - "..tostring(cont)  
 		if not t or t =='print' or t=='p' then print(text);
 		elseif t=='warn' or t=='wrn' or t=='w' then warn(text);
 		elseif t=='error' or t =='err' or t=='e' then error(text); end
 	end
 	
 	module.DebugPrint = function(cont,t)
-		if script.Parent["[ DebugMode ]"].Value then module.PluginPrint(cont,t); end
+		if Data._DebugMode.Value then module.ModPrint(cont,t); end
 	end
 	
 	module.HasProp = function(object,property)
 		if object:FindFirstChild(property) then 
-			module.PluginPrint("A child cant have the same name as one of its parent's properties. (its also bad practice)",'warn')
+			module.ModPrint("A child cant have the same name as one of its parent's properties. (its also bad practice)",'warn')
 			return; 
 		end
 		local successful = pcall(function()
