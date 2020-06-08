828a8427d74e6027f5ca108a3c200b9160b73c44
DavidRHunt <19377840+LeJuiceBOX@users.noreply.github.com>
2020-06-08 18:13:19 -0400

    Update module_helper.lua

--- module_helper.lua
+++ module_helper.lua
@@ -1,1 +1,52 @@
+--======================================================================================================
+--// Author: 	Le_JuiceBOX [Larrold#7001] [Discord ID: 263073829226676226]							   
+--// Desc:		Helper functions, used inside the main module.                                                             
+--======================================================================================================
 
+local Config = script.Parent.Configuration
+
+local module = {}
+	
+	module.PluginPrint = function(cont,t)
+		local text = "[ RUM ] - "..tostring(cont)  
+		if not t or t =='print' or t=='p' then print(text);
+		elseif t=='warn' or t=='wrn' or t=='w' then warn(text);
+		elseif t=='error' or t =='err' or t=='e' then error(text); end
+	end
+	
+	module.DebugPrint = function(cont,t)
+		if script.Parent["[ DebugMode ]"].Value then module.PluginPrint(cont,t); end
+	end
+	
+	module.HasProp = function(object,property)
+		if object:FindFirstChild(property) then 
+			module.PluginPrint("A child cant have the same name as one of its parent's properties. (its also bad practice)",'warn')
+			return; 
+		end
+		local successful = pcall(function()
+			return object[property]
+		end)
+		return successful
+	end
+	
+	module.ConfVal = function(valueName)
+		local value = Config:FindFirstChild(valueName,true) or nil
+		return value.Value
+	end 
+	
+	module.TempFolder = function()
+		local location = module.ConfVal("TempFolder_Location")
+		local name = module.ConfVal("TempFolder_Name")
+		local Folder = location:FindFirstChild(name)
+		if Folder then return Folder; end --// One already exists, return it
+		local location = location or workspace
+		local folder = Instance.new("Folder",location)
+		folder.Name = module.ConfVal("TempFolder_Name")
+		spawn(function()
+			repeat wait(0.5) until #folder:GetChildren() < 1
+			folder:Destroy()
+		end)
+		return folder
+	end
+
+return module
