84a047d2801525112aa0c8fed95a4c16525423dd
DavidRHunt <19377840+LeJuiceBOX@users.noreply.github.com>
2020-06-09 13:28:36 -0400

    ver 1.0.3

--- UPDATELOG.lua
+++ UPDATELOG.lua
@@ -1,1 +1,27 @@
+--[[
 
+  || Ver 1.0.3 (6/9/20) ||
+  > Added General.GetGameContext
+  > Fixed internal issues
+  > Removed useless declarations
+
+-------------------------------------------
+
+  || Ver 1.0.2 (6/8/20) ||
+  > Created auto-updater plugin
+  > Removed Audio.PlayID
+  > Fixed issues
+
+-------------------------------------------
+
+  || Ver 1.0.1 (6/6/20) ||
+  > Fixed issues
+
+-------------------------------------------
+
+  || Ver 1.0.0 (6/5/20) ||
+  > Initial
+
+-------------------------------------------
+
+--]]
