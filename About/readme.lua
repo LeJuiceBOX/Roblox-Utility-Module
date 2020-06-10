7a6f4aa468cee8d3e7f92563ffc8a2f61584ce2e
DavidRHunt <19377840+LeJuiceBOX@users.noreply.github.com>
2020-06-10 18:22:04 -0400

    Update and rename readme.txt to readme.lua

---
+++ About/readme.lua
@@ -0,0 +1,10 @@
+--[[
+  ==============================================
+  - Hello
+  ==============================================
+
+  Thanks for using the Roblox-Utility-Module, RUM.
+  
+  For documentation, go to [ https://github.com/LeJuiceBOX/Roblox-Utility-Module/wiki ].
+
+--]]
